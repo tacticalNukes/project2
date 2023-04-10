f60fc20b242af6692166b5a29df7a09476efaa3b
Sally Nilsson <ernl22@student.bth.se>
2023-04-10 20:41:47 +0200

    Ground

--- functional.py
+++ functional.py
@@ -7,7 +7,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 import time
-import threading
+from threading import Thread
 
 ev3 = EV3Brick()
 
@@ -21,31 +21,38 @@ COLORS = [Color.RED, Color.BLUE]
 total_time = None
 dropzones = {}
 
+ROT_SPEED = 50
+
 def getColorOfObject():
-    print("Start Looking")
-    while color_sensor.color() not in COLORS:
-        print(color_sensor.color())
-    return color_sensor.color()
+    tmp = color_sensor.color()
+    if tmp in COLORS:
+        print(tmp)
+        return tmp
+    return None
 
 def arm_down():
-    arm_raise_motor.run_until_stalled(speed=40, duty_limit=10)
+    arm_raise_motor.run_until_stalled(speed=50, duty_limit=7)
     print("Arm Down")
 
 def arm_up(waitfor_sensor):
-    t1 = threading.Thread(target = lambda : arm_raise_motor.run_until_stalled(speed=-50, then=Stop.HOLD, duty_limit=20))
-    t1.start()
-    if waitfor_sensor:
-        t2 = threading.Thread(target=getColorOfObject)
-        t2.start()
-        color = getColorOfObject()
-        print(color)
-        t2.join()
-
-    t1.join()
     print("Arm Up")
+    color = None
+    if waitfor_sensor:
+        arm_raise_motor.run(speed=-50)
+        time.sleep(3.6)
+        while color == None:
+            color = getColorOfObject()
+            if arm_raise_motor.stalled():
+                arm_raise_motor.stop()
+    arm_raise_motor.run_until_stalled(speed=-50, then=Stop.HOLD, duty_limit=20)
 
-def open_claw():
-    claw_motor.run_time(speed=-60, time=1.5*1000, then=Stop.HOLD)
+    if waitfor_sensor : return color
+
+def open_claw(withPackage):
+    if withPackage:
+        claw_motor.run_time(speed=-60, time=0.7*1000, then=Stop.HOLD)
+    else:
+        claw_motor.run_time(speed=-60, time=1.2*1000, then=Stop.HOLD)
     print("Claw open")
 
 def close_claw():
@@ -53,42 +60,50 @@ def close_claw():
     print("Claw Closed")
 
 def rotateToColor(color : Color):
-    arm_rot_motor.run_time(speed=50, time=find_key(dropzones, color), wait=True)
+    print(find_key(dropzones, color), color)
+    arm_rot_motor.run_time(speed=ROT_SPEED, time=(find_key(dropzones, color)+1) *1000, wait=True)
 
-def reset_rotation(currentDeg):
-    arm_rot_motor.run_time(speed=50, time=total_time - currentDeg, wait=True)
+def reset_rotation(color):
+    _time = (total_time-find_key(dropzones, color)+1)*1000
+    arm_rot_motor.run_time(speed=-ROT_SPEED, time=_time, wait=True)
 
 def mesure():
     """Returns degress for total arm rotation"""
     global total_time
-    _speed = 50
     start = time.time()
-    arm_rot_motor.run(speed=_speed)
+    arm_rot_motor.run(speed=ROT_SPEED)
     print(dropzones)
     while not touch_sensor.pressed():
-        if color_sensor.color() in COLORS and color_sensor.color() not in dropzones.values():
-            dropzones[time.time() - start] = color_sensor.color()    
+        tmp = color_sensor.color()
+        if tmp in COLORS and tmp not in dropzones.values():
+            dropzones[time.time() - start] = tmp   
     total_time = time.time() - start
     print("Dropzones: ")
     print(dropzones)
     arm_rot_motor.stop()
     #Returns to start position
-    arm_rot_motor.run_time(speed=-_speed, time=total_time*1000, wait=True)
+    arm_rot_motor.run_time(speed=-ROT_SPEED, time=total_time*1000, wait=True)
     #return speed * (time.time() - start)
 
 def find_key(input_dict, value):
     """Find the key for a value in a dict"""
     return next((k for k, v in input_dict.items() if v == value), None)
 
+def initiation():
+    arm_up(waitfor_sensor=False)
+    close_claw()
+    mesure()
+
 def pickup():
-    open_claw()
+    open_claw(withPackage=False)
     arm_down()
     close_claw()
-    arm_up(waitfor_sensor=True)
+    return arm_up(waitfor_sensor=True)
 
 def drop(color : Color):
     rotateToColor(color=color)
     arm_down()
-    open_claw()
+    open_claw(withPackage=True)
     arm_up(waitfor_sensor=False)
+    close_claw()
     reset_rotation(find_key(dropzones, color))
--- main.py
+++ main.py
@@ -2,18 +2,8 @@
 import functional as f
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 
-#f.arm_down()
-#f.arm_up(waitfor_sensor=False)
-f.arm_up(waitfor_sensor=False)
-f.close_claw()
-f.open_claw()
-f.arm_down()
-f.close_claw()
-f.arm_up(waitfor_sensor=True)
-# f.mesure()
-# f.open_claw()
-# f.arm_down()
+f.initiation()
 
-# alive = True
-# while alive:
-#     f.drop(color=Color.RED)
+alive = True
+while alive:
+    f.drop(f.pickup())
