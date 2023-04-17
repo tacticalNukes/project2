a070a9fc258f8d3920f82c9400bb4c50822fad8b
Sally Nilsson <ernl22@student.bth.se>
2023-04-17 10:19:03 +0200

    Add connection function

--- functional.py
+++ functional.py
@@ -3,10 +3,12 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox, LogicMailbox
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 import time
+
 ev3 = EV3Brick()
 
 arm_rot_motor = Motor(Port.C)
@@ -15,8 +17,10 @@ claw_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
 touch_sensor = TouchSensor(Port.S1)
 
-COLORS = [Color.RED, Color.BLUE]
+COLORS = [Color.YELLOW, Color.GREEN, Color.RED, Color.BLUE]
 dropzones = {}
+SERVER = BluetoothMailboxServer()
+MBOX = TextMailbox('box', SERVER)
 
 ROT_SPEED = 50
 
@@ -50,6 +54,8 @@ def open_claw():
 
 def close_claw():
     claw_motor.run_until_stalled(speed=30, then=Stop.HOLD, duty_limit=80)
+    return claw_motor.angle()
+    # Om vinkeln är mindre än 5 finns ett objekt
     print("Claw Closed")
 
 def rotateToColor(color : Color):
@@ -82,17 +88,26 @@ def find_key(input_dict, value):
     return next((k for k, v in input_dict.items() if v == value), None)
 
 def initiation():
+    connect()
     arm_up(waitfor_sensor=False)
     arm_raise_motor.reset_angle(angle=0)
     close_claw()
     claw_motor.reset_angle(angle=0)
     mesure()
 
+def checkobject_ispresent(color : Color):
+    rotateToColor(color=color)
+    open_claw()
+    arm_down()
+    close_claw()
+    
 def pickup():
     open_claw()
     arm_down()
     close_claw()
-    return arm_up(waitfor_sensor=True)
+    color = arm_up(waitfor_sensor=True)
+    ev3.light = color
+    return color
 
 def drop(color : Color):
     rotateToColor(color=color)
@@ -100,3 +115,35 @@ def drop(color : Color):
     open_claw()
     arm_up(waitfor_sensor=False)
     reset_rotation()
+
+def connect():
+  ev3.screen.clear()
+  ev3.screen.draw_text(5, 50, "Connecting...")
+  SERVER.wait_for_connection()
+  ev3.screen.clear()
+  ev3.screen.draw_text(5, 50, "Connected!")
+  ev3.speaker.beep()
+
+def sendcommands():
+  MBOX.send(MSG_DONE)
+  MBOX.wait()
+  MBOX.send(MSG_FINDPARK)
+  MBOX.wait()
+  MBOX.send(MSG_LEAVEPARK)
+  MBOX.wait()
+  MBOX.send(MSG_PARKED)
+  MBOX.wait()
+  MBOX.send(MSG_TURN)
+  MBOX.wait()
+  MBOX.send(MSG_RETURN)
+  
+  MBOX.send(MSG_DONE)
+
+MSG_DONE = "Done"
+MSG_FINDPARK = "Find Parking"
+MSG_LEAVEPARK = "Leave Parking"
+MSG_PARKED = "In Parking"
+MSG_TURN = "Turn 180"
+MSG_RETURN = "Return 180"
+
+sendcommands()
